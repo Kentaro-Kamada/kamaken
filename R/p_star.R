92a1aa083c497bc5e86cd5505443140b5d0a40a3
Kentaro Kamada <kamaken0705@gmail.com>
2020-12-02 17:21:05 +0900

    a little fix of the 'p_star' function

--- R/p_star.R
+++ R/p_star.R
@@ -3,9 +3,9 @@ p_star <- function(.data, .var){
   .var_name <- rlang::as_label(.var)
   dplyr::mutate(.data,
                 !!.var_name := dplyr::case_when(!!.var < 0.001 ~ '***',
-                                                !!.var < 0.01 ~ '**',
-                                                !!.var < 0.05 ~ '*',
-                                                !!.var < 0.1 ~ '+',
-                                                TRUE ~ '')
+                                                !!.var < 0.01 ~ ' **',
+                                                !!.var < 0.05 ~ '  *',
+                                                !!.var < 0.1 ~ '  +',
+                                                TRUE ~ '   ')
   )
 }
