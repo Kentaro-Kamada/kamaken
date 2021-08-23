d9ceca09d2acffb5491029b193c07662862d5edd
Kentaro Kamada <kamaken0705@gmail.com>
2021-08-23 17:08:07 +0900

    NAMESPACEなど修正

--- R/poLCA_BLRT.R
+++ R/poLCA_BLRT.R
@@ -1,24 +1,55 @@
+# Bootstrap Likelihood Ratio Test
+#'
+#' Bootstrap Likelihood Ratio Test with `poLCA`
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr group_gy
+#' @importFrom dplyr summarise
+#' @importFrom dplyr left_join
+#' @importFrom dplyr tibble
+#' @importFrom dplyr lag
+#' @importFrom tidyr nest
+#' @importFrom tidyr unnest
+#' @importFrom stringr str_c
+#' @importFrom purrr map
+#' @importFrom purrr map2
+#' @importFrom purrr map_dbl
+#' @importFrom purrr map2_dbl
+#' @importFrom purrr pmap
+#' @importFrom purrr pluck
+#' @importFrom furrr future_map
+#' @importFrom furrr future_pmap
+#' @importFrom furrr future_options
+#' @importFrom poLCA poLCA
+#' @importFrom poLCA poLCA.simdata
+#'
+#' @export
+#'
+
 poLCA_BLRT <- function(.poLCA_result_table, boot_size, maxiter = 6000, nrep = 3) {
   base_table <-
     .poLCA_result_table %>%
-    dplyr::select(model.no, model, glance) %>%
-    tidyr::unnest(glance) %>%
-    dplyr::select(model.no, model, df.residual, logLik:g.squared) %>%
-    dplyr::mutate(
-      モデル対比 = stringr::str_c(dplyr::lag(model.no), ' vs ', model.no),
+    select(model.no, model, glance) %>%
+    unnest(glance) %>%
+    select(model.no, model, df.residual, logLik:g.squared) %>%
+    mutate(
+      モデル対比 = str_c(lag(model.no), ' vs ', model.no),
       # deviance差を算出（最終的な検定に用いる）
-      delta_deviance = dplyr::lag(g.squared) - g.squared
+      delta_deviance = lag(g.squared) - g.squared
     )
 
   # modelから条件付応答確率，クラス割合，サンプルサイズを抜き出す
   result <-
-    dplyr::mutate(base_table,
-                  probs = purrr::map(model, purrr::pluck, 'probs') %>% dplyr::lag(),
-                  P = purrr::map(model, purrr::pluck, 'P') %>% dplyr::lag(),
-                  N = purrr::map_dbl(model, purrr::pluck, 'N') %>% dplyr::lag(),
+    mutate(base_table,
+           probs = map(model, pluck, 'probs') %>% lag(),
+           P = map(model, pluck, 'P') %>% lag(),
+           N = map_dbl(model, pluck, 'N') %>% lag(),
     ) %>%
-    dplyr::filter(model.no != min(model.no)) %>%
-    dplyr::select(!(model:g.squared))
+    filter(model.no != min(model.no)) %>%
+    select(!(model:g.squared))
 
   # 並列化の準備
   future::plan(strategy = future::multisession)
@@ -26,53 +57,52 @@ poLCA_BLRT <- function(.poLCA_result_table, boot_size, maxiter = 6000, nrep = 3)
   # パラメトリックブートストラップ
   result <-
     result %>%
-    dplyr::mutate(
+    mutate(
       bootstrap =
-        purrr::pmap(list(N, probs, P),
-                    ~{dplyr::tibble(id = 1:boot_size,
-                                    N2 = ..1,
-                                    probs2 = list(..2),
-                                    P2 = list(..3))
-                    })) %>%
-    dplyr::mutate(
+        pmap(list(N, probs, P),
+             ~{tibble(id = 1:boot_size,
+                      N2 = ..1,
+                      probs2 = list(..2),
+                      P2 = list(..3))
+             })) %>%
+    mutate(
       bootstrap =
-        furrr::future_map(bootstrap, .options = furrr::furrr_options(seed = TRUE),
-                          ~{dplyr::mutate(.,
-                                          dat = furrr::future_pmap(list(N2, probs2, P2),
-                                                                   .options = furrr::furrr_options(seed = TRUE),
-                                                                   ~{poLCA::poLCA.simdata(N = ..1, probs = ..2, P = ..3) %>%
-                                                                       .$dat}
-                                          )) %>%
-                              dplyr::select(id, dat)}
+        future_map(bootstrap, .options = furrr_options(seed = TRUE),
+                   ~{mutate(.,
+                            dat = future_pmap(list(N2, probs2, P2),
+                                              .options = furrr_options(seed = TRUE),
+                                              ~{poLCA.simdata(N = ..1, probs = ..2,
+                                                              P = ..3) %>% .$dat})
+                   ) %>% select(id, dat)}
         )) %>%
-    dplyr::select(-probs, -P, -N) %>%
+    select(-probs, -P, -N) %>%
     # ブートストラップサンプルに対して，クラス数kとクラス数k + 1の潜在クラス分析を実行
-    dplyr::mutate(
+    mutate(
       bootstrap =
-        purrr::map2(model.no, bootstrap,
-                    ~{dplyr::mutate(.y,
-                                    model.no2 = .x,
-                                    deviance_null =
-                                      purrr::map2_dbl(dat, model.no2,
-                                                      ~{poLCA::poLCA(formula = as.matrix(.x) ~ 1, data = .x,
-                                                                     nclass = .y - 1, maxiter = maxiter, nrep = nrep,
-                                                                     verbose = F) %>% .$Gsq}),
-                                    deviance_alt =
-                                      purrr::map2_dbl(dat, model.no2,
-                                                      ~{poLCA::poLCA(formula = as.matrix(.x) ~ 1, data = .x,
-                                                                     nclass = .y, maxiter = maxiter, nrep = nrep,
-                                                                     verbose = F) %>% .$Gsq}),
-                                    delta_deviance_boot = deviance_null - deviance_alt)
-                    }
+        map2(model.no, bootstrap,
+             ~{mutate(.y,
+                      model.no2 = .x,
+                      deviance_null =
+                        map2_dbl(dat, model.no2,
+                                 ~{poLCA(formula = as.matrix(.x) ~ 1, data = .x,
+                                         nclass = .y - 1, maxiter = maxiter, nrep = nrep,
+                                         verbose = F) %>% .$Gsq}),
+                      deviance_alt =
+                        map2_dbl(dat, model.no2,
+                                 ~{poLCA(formula = as.matrix(.x) ~ 1, data = .x,
+                                         nclass = .y, maxiter = maxiter, nrep = nrep,
+                                         verbose = F) %>% .$Gsq}),
+                      delta_deviance_boot = deviance_null - deviance_alt)
+             }
         )) %>%
     # p値の算出
-    tidyr::unnest(bootstrap) %>%
-    dplyr::group_by(モデル対比) %>%
-    dplyr::summarise(p.value = sum(delta_deviance < delta_deviance_boot)/boot_size)
+    unnest(bootstrap) %>%
+    group_by(モデル対比) %>%
+    summarise(p.value = sum(delta_deviance < delta_deviance_boot)/boot_size)
 
   future::plan(strategy = future::sequential)
 
-  dplyr::left_join(base_table, result, by = 'モデル対比')
+  left_join(base_table, result, by = 'モデル対比')
 }
 
 # boot_size = 1000で46分
