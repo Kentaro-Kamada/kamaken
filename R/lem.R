87c6a89feeff3eed66d7cc4bfa887616a88cd9b7
Kentaro Kamada <kamaken0705@gmail.com>
2022-09-23 20:52:01 +0900

    実装をsystemからsystem2に変更 対数尤度の取得はlem_max_loglikに任せる方針に変更

--- R/lem.R
+++ R/lem.R
@@ -1,6 +1,6 @@
 # use LEM (Vermunt 1997) thourgh R
 #'
-#' Activate LEM through `system()`.
+#' Activate LEM through `system2()`.
 #'
 #' @importFrom purrr walk
 #' @importFrom purrr map_chr
@@ -35,6 +35,12 @@ lem <- function(lat = '',
                 dat,
                 ite = '',
                 see, path) {
+
+
+  if(.Platform$OS.type == 'unix' && Sys.getenv('WINEPATH') == '') {
+    stop('please set the directory of lem95.exe to the environment variable \'WINEPATH\'')
+  }
+
   # inpとoutのディレクトリ作成
   if(!dir.exists(path)) {
     dir.create(path)
@@ -47,36 +53,30 @@ lem <- function(lat = '',
 
   # スクリプト書き出し
   walk(see, ~{write_lines(c(lat, man, con, dim, lab, mod, rec, des, sta, dum, dat, ite, .),
-                          file = str_c(path, 'inp/', str_remove(., ' '), '.inp'))})
+                          file = str_c(path, '/inp/', str_remove(., ' '), '.inp'))})
 
 
   # inputファイルとoutputファイルのパス指定
-  inp_path <- list.files(str_c(path, 'inp'), full.names = T)
+  inp_path <- list.files(str_c(path, '/inp'), full.names = T)
   out_path <- str_replace_all(inp_path, 'inp', 'out')
 
   # OS判別
-  if(.Platform$OS.type == 'windows') command <- 'lem95'
-  if(.Platform$OS.type == 'unix') command <- 'wine lem95'
+  if(.Platform$OS.type == 'windows') {
+    command <- 'lem95'
+    args <- str_c(inp_path, out_path, sep = ' ')
+    env <- character()
+  }
+  if(.Platform$OS.type == 'unix') {
+    command <- 'wine'
+    args <- str_c('lem95', inp_path, out_path, sep = ' ')
+    env <- str_c('WINEPATH=', Sys.getenv('WINEPATH'))
+  }
 
   # lem実行
-  walk(str_c(command, inp_path, out_path, sep = ' '),
-       ~{system(., wait = F)
-         Sys.sleep(0.8)})
+  walk(args,
+       ~{system2(command, args = ., env = env,
+                 stdout = F, stderr = F, wait = F)
+         Sys.sleep(0.8)}
+  )
 
-  # 最大対数尤度の読み出し
-  max_loglik <-
-    tibble(
-      filepath = out_path,
-      loglik = map_chr(filepath, read_file)
-    ) %>%
-    mutate(
-      seed = str_extract(loglik, '(?<=Seed random values   = ).+') %>%
-        parse_double(),
-      loglik = str_extract(loglik, '(?<=Log-likelihood       = ).+') %>%
-        parse_double()
-    ) %>%
-    arrange(desc(loglik)) %>%
-    mutate(rank = dense_rank(desc(loglik)))
-
-  return(max_loglik)
 }
